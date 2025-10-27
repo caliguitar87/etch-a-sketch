307e78f4d539e8cd9c70c9d1666a001601e7adcc
Krystal <235606157+caliguitar87@users.noreply.github.com>
2025-10-26 20:16:14 -0700

    My first attempt at etch-a-sketch. I used javascript to mostly do the divs and made it a dynamic sketch.

---
+++ script.js
@@ -0,0 +1,31 @@
+const container = document.querySelector(".container");
+const body =document.querySelector("body");
+function tile(num){
+const square = document.createElement("div");
+square.classList.add("square");
+square.setAttribute("style", `border:solid, 0.25px;  padding:0; width:${num-.5}px; height:${num-.5}px;`);
+
+square.addEventListener("mousemove",()=>{
+    square.style.backgroundColor ="lightblue";
+})
+
+container.appendChild(square);}
+let num =14.5;
+let num2 = 64**2;
+for (let i=0; i<num2; i++){
+    tile(num);
+}
+console.log(num2);
+const btn = document.querySelector(".btn");
+btn.addEventListener('click',()=>{
+ container.replaceChildren();
+ 
+ 
+ 
+ const answer= prompt("How many tiles would you like per side? Max is 100.")
+ const answerDiv=(960/answer)-.5;
+ for (let j =0; j<answer**2; j++){
+    tile(answerDiv);
+ }
+})
+
